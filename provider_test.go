995433b09e09d7524a1ca07afa3780f423c89bd4
pablochacin <pablochacin@gmail.com>
2025-01-29 12:37:57 +0100

    use k6build testutils (#40)
     use k6build testutils
    
    
    Signed-off-by: Pablo Chacin <pablochacin@gmail.com>

--- provider_test.go
+++ provider_test.go
@@ -12,12 +12,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	"github.com/grafana/k6build/pkg/builder"
-	"github.com/grafana/k6build/pkg/catalog"
-	"github.com/grafana/k6build/pkg/server"
-	"github.com/grafana/k6build/pkg/store/client"
-	filestore "github.com/grafana/k6build/pkg/store/file"
-	storesrv "github.com/grafana/k6build/pkg/store/server"
+	"github.com/grafana/k6build/pkg/testutils"
 	"github.com/grafana/k6deps"
 )
 
@@ -56,51 +51,19 @@ func newUnreliableProxy(upstream string, status int, failures int) http.HandlerF
 	}
 }
 
-func Test_Provider(t *testing.T) { //nolint:paralleltest
-	// 1. create local file store
-	store, err := filestore.NewFileStore(filepath.Join(t.TempDir(), "store"))
-	if err != nil {
-		t.Fatalf("store setup %v", err)
-	}
-	storeConfig := storesrv.StoreServerConfig{
-		Store: store,
-	}
+func Test_Provider(t *testing.T) { //nolint:tparallel
+	t.Parallel()
 
-	// 2. start an object store server
-	storeHandler, err := storesrv.NewStoreServer(storeConfig)
-	if err != nil {
-		t.Fatalf("store setup %v", err)
-	}
-	storeSrv := httptest.NewServer(storeHandler)
-
-	// 3. configure a local builder
-	storeClient, err := client.NewStoreClient(client.StoreClientConfig{Server: storeSrv.URL})
-	if err != nil {
-		t.Fatalf("store client setup %v", err)
-	}
-	catalog, err := catalog.NewCatalog(context.TODO(), "testdata/catalog.json")
-	if err != nil {
-		t.Fatalf("build server setup %v", err)
-	}
-	buildConfig := builder.Config{
-		Opts: builder.Opts{
-			GoOpts: builder.GoOpts{
-				CopyGoEnv: true,
-			},
+	testEnv, err := testutils.NewTestEnv(
+		testutils.TestEnvConfig{
+			WorkDir:    t.TempDir(),
+			CatalogURL: "testdata/catalog.json",
 		},
-		Catalog: catalog,
-		Store:   storeClient,
-	}
-	builder, err := builder.New(context.TODO(), buildConfig)
+	)
 	if err != nil {
-		t.Fatalf("setup %v", err)
+		t.Fatalf("test env setup %v", err)
 	}
-
-	// 5. start a builder server
-	srvConfig := server.APIServerConfig{
-		BuildService: builder,
-	}
-	buildSrv := httptest.NewServer(server.NewAPIServer(srvConfig))
+	t.Cleanup(testEnv.Cleanup)
 
 	testCases := []struct {
 		title         string
@@ -123,7 +86,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 			config: Config{
 				BuildServiceAuth: "token",
 			},
-			buildProxy: newAuthorizationProxy(buildSrv.URL, "Authorization", "Bearer token"),
+			buildProxy: newAuthorizationProxy(testEnv.BuildServiceURL(), "Authorization", "Bearer token"),
 			opts: &k6deps.Options{
 				Env: k6deps.Source{Name: "K6_DEPS", Contents: []byte("k6=v0.50.0")},
 			},
@@ -136,7 +99,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 					"Custom-Auth": "token",
 				},
 			},
-			buildProxy: newAuthorizationProxy(buildSrv.URL, "Custom-Auth", "token"),
+			buildProxy: newAuthorizationProxy(testEnv.BuildServiceURL(), "Custom-Auth", "token"),
 			opts: &k6deps.Options{
 				Env: k6deps.Source{Name: "K6_DEPS", Contents: []byte("k6=v0.50.0")},
 			},
@@ -147,7 +110,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 			config: Config{
 				BuildServiceAuth: "",
 			},
-			buildProxy: newAuthorizationProxy(buildSrv.URL, "Authorization", "Bearer token"),
+			buildProxy: newAuthorizationProxy(testEnv.BuildServiceURL(), "Authorization", "Bearer token"),
 			opts: &k6deps.Options{
 				Env: k6deps.Source{Name: "K6_DEPS", Contents: []byte("k6=v0.50.0")},
 			},
@@ -155,7 +118,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 		},
 		{
 			title:         "test download using proxy",
-			downloadProxy: newTransparentProxy(storeSrv.URL),
+			downloadProxy: newTransparentProxy(testEnv.StoreServiceURL()),
 			opts: &k6deps.Options{
 				Env: k6deps.Source{Name: "K6_DEPS", Contents: []byte("k6=v0.50.0")},
 			},
@@ -180,7 +143,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 					Authorization: "token",
 				},
 			},
-			downloadProxy: newAuthorizationProxy(storeSrv.URL, "Authorization", "Bearer token"),
+			downloadProxy: newAuthorizationProxy(testEnv.StoreServiceURL(), "Authorization", "Bearer token"),
 			opts: &k6deps.Options{
 				Env: k6deps.Source{Name: "K6_DEPS", Contents: []byte("k6=v0.50.0")},
 			},
@@ -193,7 +156,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 					Authorization: "",
 				},
 			},
-			downloadProxy: newAuthorizationProxy(buildSrv.URL, "Authorization", "Bearer token"),
+			downloadProxy: newAuthorizationProxy(testEnv.StoreServiceURL(), "Authorization", "Bearer token"),
 			opts: &k6deps.Options{
 				Env: k6deps.Source{Name: "K6_DEPS", Contents: []byte("k6=v0.50.0")},
 			},
@@ -201,7 +164,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 		},
 		{
 			title:         "test download with default retries",
-			downloadProxy: newUnreliableProxy(storeSrv.URL, http.StatusServiceUnavailable, 1),
+			downloadProxy: newUnreliableProxy(testEnv.StoreServiceURL(), http.StatusServiceUnavailable, 1),
 			opts: &k6deps.Options{
 				Env: k6deps.Source{Name: "K6_DEPS", Contents: []byte("k6=v0.50.0")},
 			},
@@ -209,7 +172,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 		{
 			title:         "test we don't retry forever",
 			config:        Config{DownloadConfig: DownloadConfig{Retries: 1}},
-			downloadProxy: newUnreliableProxy(storeSrv.URL, http.StatusServiceUnavailable, math.MaxInt),
+			downloadProxy: newUnreliableProxy(testEnv.StoreServiceURL(), http.StatusServiceUnavailable, math.MaxInt),
 			opts: &k6deps.Options{
 				Env: k6deps.Source{Name: "K6_DEPS", Contents: []byte("k6=v0.50.0")},
 			},
@@ -221,7 +184,7 @@ func Test_Provider(t *testing.T) { //nolint:paralleltest
 		t.Run(tc.title, func(t *testing.T) {
 			// by default, we use the build service, but if there's a
 			// proxy defined, we use it
-			testSrvURL := buildSrv.URL
+			testSrvURL := testEnv.BuildServiceURL()
 			if tc.buildProxy != nil {
 				testSrv := httptest.NewServer(tc.buildProxy)
 				defer testSrv.Close()
